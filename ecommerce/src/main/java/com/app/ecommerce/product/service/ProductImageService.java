b020cd97d4c970dfb35aa7bdad329fe3217c4e0c
Lucas <lucasjlima2006@gmail.com>
2026-08-01 21:13:58 -0300

    feat: (product) adding security and file precautions

--- ecommerce/src/main/java/com/app/ecommerce/product/service/ProductImageService.java
+++ ecommerce/src/main/java/com/app/ecommerce/product/service/ProductImageService.java
@@ -4,10 +4,14 @@ import com.app.ecommerce.aws.service.S3ImageService;
 import com.app.ecommerce.product.entity.Product;
 import com.app.ecommerce.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.apache.tika.Tika;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.io.IOException;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
@@ -16,13 +20,24 @@ public class ProductImageService {
 
     private final S3ImageService s3ImageService;
     private final ProductRepository productRepository;
+    private final Tika tika;
 
-    public void upload(UUID productId, MultipartFile multipartFile) throws IOException {
-        String key = "products/" + productId + "/" + UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
+    public void upload(UUID productId, MultipartFile file) throws IOException {
+        if (file.isEmpty()) {
+            throw new IllegalArgumentException("File is empty");
+        }
 
-        s3ImageService.uploadImg(key, multipartFile.getBytes(), multipartFile.getContentType());
+        String mimeType = tika.detect(file.getInputStream());
+
+        if (!ALLOWED_TYPES.contains(mimeType)) {
+            throw new IllegalArgumentException("File type not allowed: " + mimeType);
+        }
+
+        String key = "products/" + productId + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+
+        s3ImageService.uploadImg(key, file.getBytes(), file.getContentType());
         Product product = productRepository.findById(productId).orElseThrow(
-                () -> new RuntimeException("Product not found")
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Product not found")
         );
         product.setImgKey(key);
         productRepository.save(product);
@@ -30,8 +45,16 @@ public class ProductImageService {
 
     public String generatePresignedUrl(UUID productId) {
         Product product = productRepository.findById(productId).orElseThrow(
-                () -> new RuntimeException("Product not found")
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found")
         );
         return s3ImageService.generatePresignedUrl(product.getImgKey());
     }
+
+
+    private final Set<String> ALLOWED_TYPES = Set.of(
+            "image/jpeg",
+            "image/png",
+            "image/gif",
+            "image/webp"
+    );
 }
