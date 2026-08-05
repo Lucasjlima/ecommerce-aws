3cf4b5afe77ba11a0bdf3693e20da5ea29bb8b88
Lucas <lucasjlima2006@gmail.com>
2026-08-05 15:15:49 -0300

    feat: (cart) cart controller created

---
+++ ecommerce/src/main/java/com/app/ecommerce/cart/controller/CartController.java
@@ -0,0 +1,32 @@
+package com.app.ecommerce.cart.controller;
+
+import com.app.ecommerce.cart.dto.request.CartItemRequest;
+import com.app.ecommerce.cart.dto.response.CartResponse;
+import com.app.ecommerce.cart.entity.Cart;
+import com.app.ecommerce.cart.mapper.CartMapper;
+import com.app.ecommerce.cart.service.CartService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/api/v1/cart")
+@RequiredArgsConstructor
+public class CartController {
+    private final CartService cartService;
+
+    @PostMapping
+    public ResponseEntity<CartResponse> addProductIntoCart(@RequestBody @Valid CartItemRequest cartItemRequest) {
+        Cart cart = cartService.addProductIntoCart(cartItemRequest);
+        return ResponseEntity
+                .created(URI.create("/api/v1/cart/" + cart.getId()))
+                .body(CartMapper.toResponse(cart));
+    }
+
+}
